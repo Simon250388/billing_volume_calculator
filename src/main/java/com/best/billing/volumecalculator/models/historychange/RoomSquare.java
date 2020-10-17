6627f26021146d5887f1c5024a1f3e6940571dea
Симанов Алексей <a.v.simonov@mail.ru>
2020-10-17 16:18:18 +0400

    fix test room square

--- src/main/java/com/best/billing/volumecalculator/models/historychange/RoomSquare.java
+++ src/main/java/com/best/billing/volumecalculator/models/historychange/RoomSquare.java
@@ -23,25 +23,25 @@ import javax.persistence.*;
 @Table(name = "room_squares")
 @NamedQuery(name = RoomSquare.FIND_ONE_LAST_COMMON_SQUARE_BY_KEY_ROOM_ID,
         query = "FROM RoomSquare c" +
-                "         WHERE c.keyRoom =:keyRoomId" +
+                "         WHERE c.keyRoom.id =:keyRoomId" +
                 "         AND c.squareType.id =" + SquareType.COMMON_SQUARE_TYPE_ID + "" +
                 "         AND c.period = (SELECT MAX(c.period)" +
                 "                         FROM RoomSquare c" +
-                "                         WHERE c.keyRoom.id =:keyRoomId)" +
-                "                         AND c.squareType.id =" + SquareType.COMMON_SQUARE_TYPE_ID + "")
+                "                         WHERE c.keyRoom.id =:keyRoomId" +
+                "                         AND c.squareType.id =" + SquareType.COMMON_SQUARE_TYPE_ID + ")")
 public class RoomSquare extends BaseHistory {
     public static final String FIND_ONE_LAST_COMMON_SQUARE_BY_KEY_ROOM_ID = "RoomSquare.findOneLastCommonSquareByKeyRoomId";
     /**
      * Ключ помещения
      */
     @ManyToOne
-    @JoinColumn(name = "key_room_id")
+    @JoinColumn(name = "key_room_id", nullable = false)
     private KeyRoom keyRoom;
     /**
      * Тип площади
      */
     @ManyToOne
-    @JoinColumn(name = "square_type_id")
+    @JoinColumn(name = "square_type_id", nullable = false)
     private SquareType squareType;
     /**
      * Значение площади
--- src/test/java/com/best/billing/volumecalculator/repositories/historychange/RoomSquareRepositoryTest.java
+++ src/test/java/com/best/billing/volumecalculator/repositories/historychange/RoomSquareRepositoryTest.java
@@ -1,14 +1,85 @@
 package com.best.billing.volumecalculator.repositories.historychange;
 
+import com.best.billing.volumecalculator.models.catalog.Building;
+import com.best.billing.volumecalculator.models.entity.KeyRoom;
+import com.best.billing.volumecalculator.models.enums.SquareType;
+import com.best.billing.volumecalculator.models.historychange.RoomSquare;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import java.util.Calendar;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@Transactional
 class RoomSquareRepositoryTest {
+    @Autowired
+    private RoomSquareRepository repository;
+
+    @Autowired
+    private EntityManager em;
 
     @Test
-    void findOneLastCommonSquareByKeyRoomId() {
+    void When_Table_Is_Empty_Then_FindOneLastCommonSquareByKeyRoomId_Should_Be_Empty() {
+        Optional<RoomSquare> commonSquare = repository.findOneLastCommonSquareByKeyRoomId(anyLong());
+        assertTrue(commonSquare.isEmpty());
     }
 
     @Test
-    void findOneLastCommonSquareByKeyRoomIdAsync() {
+    void When_Table_Contains_MAny_Rows_Then_FindOneLastCommonSquareByKeyRoomId_Should_Be_Return_Row_Which_Have_Max_Period() {
+        Building building = Building.builder()
+                .description(anyString())
+                .build();
+
+        em.persist(building);
+
+        KeyRoom keyRoom = KeyRoom.builder()
+                .privateSector(anyBoolean())
+                .room(null)
+                .building(building)
+                .build();
+
+        em.persist(keyRoom);
+
+        Calendar calendar;
+
+        calendar = Calendar.getInstance();
+        calendar.set(2019, 1, 1);
+
+        em.persist(RoomSquare.builder()
+                .keyRoom(keyRoom)
+                .period(calendar.getTime())
+                .squareType(
+                        SquareType.builder().id(SquareType.COMMON_SQUARE_TYPE_ID).build()
+                )
+                .value(30)
+                .build());
+
+        calendar = Calendar.getInstance();
+        calendar.set(2020, 1, 1);
+
+        em.persist(RoomSquare.builder()
+                .keyRoom(keyRoom)
+                .period(calendar.getTime())
+                .squareType(
+                        SquareType.builder().id(SquareType.COMMON_SQUARE_TYPE_ID).build()
+                )
+                .value(20)
+                .build());
+
+        Optional<RoomSquare> commonSquare = repository.findOneLastCommonSquareByKeyRoomId(keyRoom.getId());
+        assertFalse(commonSquare.isEmpty());
+        RoomSquare dataRow = commonSquare.get();
+        assertEquals(calendar.getTime(), dataRow.getPeriod());
+        assertEquals(20, dataRow.getValue());
     }
 }
