3dff56f9a5f8c180d6033b89dc42c1094db92157
Симанов Алексей <a.v.simonov@mail.ru>
2021-09-30 22:00:47 +0400

    add cucumber integration test

---
+++ cucumber-test/src/test/java/org/billing/cucumber/ContainerBox.java
@@ -0,0 +1,21 @@
+package org.billing.cucumber;
+
+import java.io.File;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+
+public class ContainerBox {
+
+  public static Integer API_PORT = 8080;
+
+  @Container
+  public static DockerComposeContainer compose =
+      new DockerComposeContainer(new File("src/main/resources/docker-compose.yml"))
+          .withLocalCompose(true)
+          .withExposedService("api", API_PORT, Wait.forListeningPort());
+
+  public static void initTestContainers() {
+    compose.start();
+  }
+}
