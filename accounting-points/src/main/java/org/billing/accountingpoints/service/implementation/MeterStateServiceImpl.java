4ede562c12682b926e8832b14a4c04dbb597dc65
Симанов Алексей <a.v.simonov@mail.ru>
2021-10-17 21:21:59 +0400

    fix accounting point start up

--- accounting-points/src/main/java/org/billing/accountingpoints/service/implementation/MeterStateServiceImpl.java
+++ accounting-points/src/main/java/org/billing/accountingpoints/service/implementation/MeterStateServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.billing.accountingpoints.dto.AccountingPointMeterStateDto;
 import org.billing.accountingpoints.repository.AccountingPointMeterStateRepository;
 import org.billing.accountingpoints.service.MeterStateService;
@@ -12,15 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
 public class MeterStateServiceImpl implements MeterStateService {
-
-  @Autowired
-  public MeterStateServiceImpl(AccountingPointMeterStateRepository meterStateDao) {}
+  private final AccountingPointMeterStateRepository meterStateDao;
 
   @Override
-  public CompletableFuture<Set<AccountingPointMeterStateDto>>
-      currentStateByEntityServiceIdAsync(
-          Set<UUID> accountingPointKeyRoomServiceEntities, Instant period) {
+  public CompletableFuture<Set<AccountingPointMeterStateDto>> currentStateByEntityServiceIdAsync(
+      Set<UUID> accountingPointKeyRoomServiceEntities, Instant period) {
     return CompletableFuture.completedFuture(Collections.emptySet());
   }
 }
